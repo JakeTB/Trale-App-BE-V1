398bb0f22c3396701180a8bc7fdb63d5d335e856
tb440167 <terryblack001@gmail.com>
2020-02-03 14:14:16 +0000

    change to knexfile to allow multiple users working

--- db/connection.js
+++ db/connection.js
@@ -4,6 +4,6 @@ const knex = require("knex");
 const dbConfig =
   ENV === "production"
     ? { client: "pg", connection: process.env.DATABASE_URL }
-    : require("../knexfile");
+    : require("../knexfile1");
 
 module.exports = knex(dbConfig);
