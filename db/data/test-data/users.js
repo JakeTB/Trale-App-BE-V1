14ddc609ff5d52f19913e3d534e9ae8346df9d95
JakeTB <jtilbybaxter@gmail.comgit config --global user.name Jakegit config --global user.email jtilbybaxter@gmail.com>
2020-02-05 15:22:31 +0000

    Sorted out problems with the tests

--- db/data/test-data/users.js
+++ db/data/test-data/users.js
@@ -1,12 +1,18 @@
 module.exports = [
-  { username: "Adam", avatar: "https://i.vimeocdn.com/video/55934283_640.jpg" },
+  {
+    username: "Adam",
+    avatar: "https://i.vimeocdn.com/video/55934283_640.jpg",
+    bio: ""
+  },
   {
     username: "Jake",
     avatar:
-      "https://i.pinimg.com/originals/cb/c0/6d/cbc06d3bcfae8c8460a1af2ccf816102.jpg"
+      "https://i.pinimg.com/originals/cb/c0/6d/cbc06d3bcfae8c8460a1af2ccf816102.jpg",
+    bio: ""
   },
   {
     username: "Roan",
-    avatar: "https://pbs.twimg.com/media/EBTuo_EVUAA82Uo.jpg"
+    avatar: "https://pbs.twimg.com/media/EBTuo_EVUAA82Uo.jpg",
+    bio: ""
   }
 ];
--- db/migrations/1580718981317_createdPubs.js
+++ db/migrations/1580718981317_createdPubs.js
@@ -1,19 +1,22 @@
 exports.up = function(knex) {
-  return knex.schema.createTable("pubs", pubsTable => {
-    pubsTable.increments("id").primary();
-    pubsTable.text("pub_picture");
-    pubsTable.text("pub_description");
-    pubsTable.string("pub_name").notNullable();
-    pubsTable.text("pub_address").notNullable();
-    pubsTable.float("lat").notNullable();
-    pubsTable.float("lng").notNullable();
-    pubsTable
-      .integer("routes_id")
-      .references("id")
-      .inTable("routes")
-      .notNull()
-      .onDelete("cascade");
-  });
+  return knex.schema
+    .createTable("pubs", pubsTable => {
+      pubsTable.increments("id").primary();
+      pubsTable.text("pub_picture");
+      pubsTable.text("pub_description");
+      pubsTable.string("pub_name").notNullable();
+      pubsTable.text("pub_address").notNullable();
+      pubsTable.float("lat").notNullable();
+      pubsTable.float("lng").notNullable();
+      pubsTable
+        .integer("routes_id")
+        .references("id")
+        .inTable("routes")
+        .onDelete("cascade");
+    })
+    .then(() => {
+      console.log("Pubs table created");
+    });
 };
 
 exports.down = function(knex) {
